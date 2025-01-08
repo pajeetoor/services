4db6b7a92587aa4143bcbdf5350175ed1004496d
paj33tooor <139661626+pajeetoor@users.noreply.github.com>
2025-01-08 02:07:31 -0500

    Create context.ts

---
+++ context.ts
@@ -0,0 +1,46 @@
+interface ChaosContext {
+  mohallaId: string;
+  bakwasActivity: string;
+  gullyMentality: {
+    dostiPositivity: number;
+    faltuEngagement: number;
+  };
+  culturalTadka: {
+    melaEvents: string[];
+    puraniParampara: string[];
+  };
+}
+
+interface MadnessStatus {
+  soRahaHai: boolean;
+  chaiWalaCount: number;
+  totalMirchMasalaPatterns: number;
+  totalGalatFaisle: number;
+  uptimeSeconds: number;
+  lastChaiBreak: string;
+  performanceMetrics: {
+    regretKaSamay: number;
+    jugaduGuesses: number;
+    disasterKaRate: number;
+  };
+}
+
+interface MinionQuirks {
+  traits: {
+    jugaaduPan: number;
+    bhoolpan: number;
+    cheekhShor: number;
+    khilafat: number;
+    naachGaanaEnergy: number;
+  };
+  shauk: string[];
+  behesKaAndaaz: string;
+}
+
+interface MinionScorecard {
+  galiDena: number;
+  luckyFaisle: number;
+  sikhnayKiRaftar: number;
+  dhalneKiKshamata: number;
+  bharosaInChaos: number;
+}
